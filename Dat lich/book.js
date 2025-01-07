e05e412ed8b74e02867fd004092af1aa41559ba3
DuongVanTra <hayatekawasaki2016@gmail.com>
2025-01-07 17:03:44 +0700

    Chua xong dang ky nua troi oi, toi muon hoc su dung them database nua

--- Dat lich/book.js
+++ Dat lich/book.js
@@ -29,20 +29,55 @@ function unload(){
     document.getElementById('load').style.display='none';
 }
 
+/* ========================================================================= */
+/*                              M A I N                                      */
+/* ========================================================================= */
+
 let trangthai=1;
+let rows = 4; 
+let cols = 8; 
+let inforbs = new Array(rows).fill('Bất kì').map(() => new Array(cols).fill('Bất kì'));
+inforbs[1][2]='Nữ';
+inforbs[1][3]='Nam';
+for (let i = 0; i < cols; i++) { inforbs[3][i] = 'Bất kì ngày trong tuần';}
+
+inforbs[0][4]='Dương Văn Trà';
+inforbs[1][4]='Nam';
+inforbs[2][4]='Tiến sĩ';
+inforbs[3][4]='Thứ 2/4/6/chủ nhật';
+
 
 function updateDisplay() { 
     document.getElementsByClassName('b1')[0].style.display='none';
     document.getElementsByClassName('b2')[0].style.display='none';
     document.getElementsByClassName('b3')[0].style.display='none';
+    document.getElementsByClassName('tron')[0].style.backgroundColor='rgb(84, 109, 115)';
+    document.getElementsByClassName('tron')[1].style.backgroundColor='rgb(84, 109, 115)';
+    document.getElementsByClassName('tron')[2].style.backgroundColor='rgb(84, 109, 115)';
 
     if (trangthai == 1) { 
-        document.getElementsByClassName('b1')[0].style.display = 'block'; } 
-    else if (trangthai == 2) { 
-        document.getElementsByClassName('b2')[0].style.display = 'block'; } 
-    else if (trangthai == 3) { 
-        document.getElementsByClassName('b3')[0].style.display = 'block'; }
+        document.getElementsByClassName('b1')[0].style.display = 'block'; 
+        document.getElementById('t1').style.backgroundColor = 'rgb(0, 208, 255)';
+        document.getElementById('t1').style.borderColor = 'red';
+        document.getElementById('h2').style.backgroundColor = 'black'; 
+        document.getElementById('t2').style.borderColor = 'black'; 
+    } 
     
+    else if (trangthai == 2) { 
+        document.getElementsByClassName('b2')[0].style.display = 'block'; 
+        document.getElementById('h2').style.backgroundColor = 'red'; 
+        document.getElementById('t2').style.backgroundColor = 'rgb(0, 208, 255)';
+        document.getElementById('t2').style.borderColor = 'red';
+        document.getElementById('h3').style.backgroundColor = 'black'; 
+        document.getElementById('t3').style.borderColor = 'black'; 
+    } 
+        
+    else if (trangthai == 3) { 
+        document.getElementsByClassName('b3')[0].style.display = 'block';  
+        document.getElementById('h3').style.backgroundColor = 'red'; 
+        document.getElementById('t3').style.backgroundColor = 'rgb(0, 208, 255)';
+        document.getElementById('t3').style.borderColor = 'red';
+    }
 }
 function next(){
     trangthai+=1;
@@ -55,11 +90,58 @@ function back(){
     updateDisplay();
 }
 
-function choiced(n){
-    let a = document.getElementById('find');
-    for(let i=0;i<n;i++){
+function nd(n){
+    let a = document.getElementById('f1');
+    for(let i=1;i<n;i++){
         a = a.nextElementSibling;
     }
     let value = a.textContent;
-    document.getElementById('find').value=value;
+    document.getElementById('findnd').value=value;
+}
+function dc(n){
+    let a = document.getElementById('f2');
+    for(let i=1;i<n;i++){
+        a = a.nextElementSibling;
+    }
+    let value = a.textContent;
+    document.getElementById('finddc').value=value;
+}
+function cs(n){
+    let a = document.getElementById('f3');
+    for(let i=1;i<n;i++){
+        a = a.nextElementSibling;
+    }
+    let value = a.textContent;
+    document.getElementById('findcs').value=value;
+}
+function bs(n){
+    let a = document.getElementById('f4');
+    for(let i=1;i<n;i++){
+        a = a.nextElementSibling;
+    }
+    let value = a.textContent;
+    document.getElementById('findbs').value=value;
+    
+    var [name,gender,learn,dates] = infor(n);
+    var img='../Images/doctor.png';
+    
+    if(gender=='Nam'){
+        img='../Images/male.jpg';
+    }
+    else if(gender=='Nữ') {
+        img='../Images/female.jpg'
+    }
+
+    document.getElementById('name').textContent = name;
+    document.getElementById('gender').textContent = gender;
+    document.getElementById('learn').textContent = learn;
+    document.getElementById('dates').textContent = dates;
+    document.getElementById('img').src = img;
+}
+function infor(n){
+    let name = inforbs[0][n];
+    let gender = inforbs[1][n];
+    let learn = inforbs[2][n];
+    let dates = inforbs[3][n];
+    return [name,gender,learn,dates];
 }
